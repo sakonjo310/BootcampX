fc1677b3589727b5aedac7c46ad70fe31917544d
Satoe Sakonjo <sakonjo310@gmail.com>
2022-11-16 14:47:42 -0800

    Wrong directory

---
+++ bootcamp_app/students.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT id, name, cohort_id
+FROM students
+LIMIT 5;
+`)
+.then(res => {
+  console.log(res);
+})
+.catch(err => console.error('query error', err.stack));
+
